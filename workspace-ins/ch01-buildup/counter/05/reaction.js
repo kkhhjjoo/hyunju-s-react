b86495b140759e30ad9072f44a87c610fd0eb373
kilyong jeong <uzoolove@gmail.com>
2025-11-26 14:31:03 +0900

    05 createRoot(), render() 함수 만들기

---
+++ workspace-ins/ch01-buildup/counter/05/reaction.js
@@ -0,0 +1,36 @@
+const reaction = {
+  createElement: (tag, props, ...children) => {
+    // 요소 노드 생성
+    const elem = document.createElement(tag);
+
+    // 속성 추가
+    if(props){
+      for(const attrName in props){
+        const value = props[attrName];
+        elem.setAttribute(attrName, value);
+      }
+    }
+
+    // 자식 노드 추가
+    for(let child of children){
+      if(typeof child === 'string' || typeof child === 'number'){
+        child = document.createTextNode(child);
+      }
+      elem.appendChild(child);
+    }
+
+    // 요소 노드 반환
+    return elem;
+  },
+  // 루트 노드를 관리하는 객체를 반환
+  createRoot: (rootNode) => {
+    return {
+      render: (appFn) => {
+        // appFn을 실행한 결과 노드를 루트 노드의 자식으로 렌더링 한다.
+        rootNode.appendChild(appFn());
+      }
+    };
+  }
+};
+
+export default reaction;
