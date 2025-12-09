2ff22771c4e52b91982cd69ca3fbcb4e24d55af5
kilyong jeong <uzoolove@gmail.com>
2025-12-09 14:48:08 +0900

    02 useEffect - side effect 관리...

--- workspace-ins/ch04-hooks/02-useEffect/src/components/Counter.tsx
+++ workspace-ins/ch04-hooks/02-useEffect/src/components/Counter.tsx
@@ -1,5 +1,5 @@
 import Button from "@/components/Button";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 interface CounterProps {
   children: string;
@@ -30,10 +30,35 @@ function Counter({ children }: CounterProps) {
   // 리액트 규칙: 렌더링 중에 상태가 변경되면 안됨
 
   // TODO 1. 1초 후에 handleUp()을 호출해서 자동으로 값을 1회 증가
+  // setTimeout(() => {
+  //   console.log('무한 렌더링');
+  //   handleUp();
+  // }, 1000);
+
+  console.log('input 요소', document.querySelector('input'));
+  useEffect(() => {
+    console.log('useEffect 내부에서 input 요소', document.querySelector('input'));
+    // document.querySelector('input')?.focus();
+  });
+
+  // useEffect(() => { // setup
+  //   console.log('\t\tCounter 마운트됨');
+  //   setTimeout(() => {
+  //     handleUp();
+  //   }, 1000);
+  //   console.log('dependencies에 빈배열을 지정하면 마운트된 후에 한번만 호출됨');
+  // }, []);
 
   // TODO 2. 증감치(step)가 수정되면 1초 후에 handleUp()을 호출해서 자동으로 값을 1회 증가
 
   // TODO 3. 1초마다 handleUp()을 호출해서 자동으로 값을 계속 증가
+  useEffect(() => { // setup
+    console.log('\t\tCounter 마운트 또는 업데이트 됨');
+    setTimeout(() => {
+      handleUp();
+    }, 1000);
+    console.log('dependencies를 생략하면 마운트된 후와 업데이트된 후에도 호출됨');
+  });
 
   return (
     <div id="counter">
