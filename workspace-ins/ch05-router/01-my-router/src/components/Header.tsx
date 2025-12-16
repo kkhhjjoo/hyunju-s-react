53a62ecd5938dec94cc8f10ddabfcba8ac02e553
kilyong jeong <uzoolove@gmail.com>
2025-12-16 13:41:19 +0900

    리액트 라우터...

--- workspace-ins/ch05-router/01-my-router/src/components/Header.tsx
+++ workspace-ins/ch05-router/01-my-router/src/components/Header.tsx
@@ -1,7 +1,26 @@
 function Header() {
+  const handleClick = (e: React.MouseEvent<HTMLAnchorElement>) => {
+    // 브라우저의 기본동작 취소
+    e.preventDefault();
+
+    // History API를 사용해서 URL 변경하고 history에 쌓는다.
+    window.history.pushState(null, '', e.currentTarget.href);
+
+    // dispatchEvent: 이벤트를 수동으로 발생
+    // new PopStateEvent(): 브라우저의 뒤로/앞으로 가기 등의 이벤트를 생성
+    window.dispatchEvent(new PopStateEvent('popstate'));
+  };
+
   return (
     <>
-      
+      <header>
+        <h1>리액트 라우터 - 01 클라이언트 라우팅 직접 구현 - SPA</h1>
+        <a className="menu-dark" href="home" onClick={handleClick}>home</a>
+        <br/>
+        <a className="menu" href="page1" onClick={handleClick}>page1</a>
+        <br/>
+        <a className="menu" href="page2" onClick={handleClick}>page2</a>
+      </header>
     </>
   )
 }
--- workspace-ins/ch05-router/01-my-router/src/pages/Page1.tsx
+++ workspace-ins/ch05-router/01-my-router/src/pages/Page1.tsx
@@ -1,15 +1,10 @@
+import Header from "@components/Header";
+
 function Page1() {
   console.log('Page1 렌더링');
   return (
     <>
-      <header>
-        <h1>리액트 라우터 - 01 클라이언트 라우팅 직접 구현</h1>
-        <a className="menu" href="home.html">home</a>
-        <br/>
-        <a className="menu-dark" href="page1.html">page1</a>
-        <br/>
-        <a className="menu" href="page2.html">page2</a>
-      </header>
+      <Header />
       <h2>SPA Page1</h2>
     </>
   )
