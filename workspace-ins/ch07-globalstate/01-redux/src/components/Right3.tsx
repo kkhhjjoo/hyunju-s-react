573681869df3debf937728bb9bc705d740c10f83
kilyong jeong <uzoolove@gmail.com>
2025-12-23 16:28:42 +0900

    크리스마스 버전

--- workspace-ins/ch07-globalstate/01-redux/src/components/Right3.tsx
+++ workspace-ins/ch07-globalstate/01-redux/src/components/Right3.tsx
@@ -16,7 +16,7 @@ function Right3() {
       <h3>Right3</h3>
       <button onClick={ () => dispatch(counterActionCreator.countDown(1)) }>-1</button>
       <button onClick={ () => dispatch(counterActionCreator.countReset()) }>0</button>
-      <button onClick={ () => dispatch(counterActionCreator.countUp(1)) }>+1</button>
+      <button onClick={ () => dispatch({ type: 'countUp', payload: { step: 1 } }) }>+1</button>
     </div>
   );
 }
