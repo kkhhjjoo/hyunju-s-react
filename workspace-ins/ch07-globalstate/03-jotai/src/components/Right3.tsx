9a4fc6b84dd653ef8935c376faea8961566349dc
kilyong jeong <uzoolove@gmail.com>
2025-12-24 11:22:47 +0900

    jotai 기본 사용

--- workspace-ins/ch07-globalstate/03-jotai/src/components/Right3.tsx
+++ workspace-ins/ch07-globalstate/03-jotai/src/components/Right3.tsx
@@ -1,13 +1,25 @@
+import { countAtom } from '@/jotai/atoms';
+import { useSetAtom } from 'jotai';
 import { useEffect } from 'react';
 
 function Right3() {
   useEffect(()=>{
     console.log('#### Right3 렌더링.');
   });
+
+  // useAtom: useState처럼 getter, setter를 배열로 반환. 구독됨
+  // const [count, setCount] = useAtom(countAtom);
+
+  // useSetAtom: setter만 사용
+  const setCount = useSetAtom(countAtom);
+  const countUp = (step: number) => {
+    setCount((count) => count + step);
+  };
+
   return (
     <div>
       <h3>Right3</h3>
-      <button onClick={ () => {} }>+1</button>
+      <button onClick={ () => countUp(1) }>+1</button>
     </div>
   );
 }
---
+++ workspace-ins/ch07-globalstate/03-jotai/src/jotai/atoms.ts
@@ -0,0 +1,3 @@
+import { atom } from "jotai";
+
+export const countAtom = atom(5); // 상태 하나를 정의(초기값)
