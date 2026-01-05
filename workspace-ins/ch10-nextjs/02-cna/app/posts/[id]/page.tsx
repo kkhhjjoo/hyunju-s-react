cda7635da755a49145dc4ccaa1c4167cb5681b40
kilyong jeong <uzoolove@gmail.com>
2026-01-05 14:49:21 +0900

    generateMetadata 추가

--- workspace-ins/ch10-nextjs/02-cna/app/posts/[id]/page.tsx
+++ workspace-ins/ch10-nextjs/02-cna/app/posts/[id]/page.tsx
@@ -1,3 +1,19 @@
+import { Metadata } from "next";
+
+export async function generateMetadata(): Promise<Metadata> {
+  // TODO: API 서버 호출 필요
+
+  const data = {
+    title: `1번 게시물`,
+    content: '게시판 이용 수칙입니다.',
+  };
+
+  return {
+    title: data.title,
+    description: data.content,
+  }
+}
+
 export default function PostInfo(){
   return (
     <h1>1번 게시물 상세 조회</h1>
